2473d3ad619eef2151b84a5601cbf8a23d08e552
Alexey <48336899+alloself@users.noreply.github.com>
2020-08-09 21:57:06 +0300

    Delete 0b91c337b280e7c887980e59a5e1b1ed78cf2852.php

--- storage/framework/views/0b91c337b280e7c887980e59a5e1b1ed78cf2852.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<h1>Страница не найдена.</h1>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.site', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH F:\projects\openserver\domains\cms\resources\views\site/pages/404.blade.php ENDPATH**/ ?>
